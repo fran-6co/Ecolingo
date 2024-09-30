b40d6a36bbe0b04285c5e25ba1833ff2583bb4b2
Fran O <contact@fran-ochoa.es>
2024-09-30 11:44:54 +0200

    first commit

---
+++ Ecolingo/Ecolingo/EcolingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcolingoApp.swift
+//  Ecolingo
+//
+//  Created by Fran Ochoa on 30/9/24.
+//
+
+import SwiftUI
+
+@main
+struct EcolingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
