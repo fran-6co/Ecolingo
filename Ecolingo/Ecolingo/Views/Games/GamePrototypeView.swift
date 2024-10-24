953781fc308f5359bc7fcc3c52aa1f362bb0c4d2
Fran O <contact@fran-ochoa.es>
2024-10-24 12:11:24 +0200

    Preparation for prototype

---
+++ Ecolingo/Ecolingo/Views/Games/GamePrototypeView.swift
@@ -0,0 +1,18 @@
+//
+//  GamePrototypeView.swift
+//  Ecolingo
+//
+//  Created by Fran Ochoa on 24/10/24.
+//
+
+import SwiftUI
+
+struct GamePrototypeView: View {
+    var body: some View {
+        Text("use this view to prototype a matching game")
+    }
+}
+
+#Preview {
+    GamePrototypeView()
+}
