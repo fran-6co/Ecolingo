452d3220cf41394c1ffd3ad4f31e1bae3183638e
Fran O <contact@fran-ochoa.es>
2024-10-24 12:05:35 +0200

    predesign of cards and home tab

--- Ecolingo/Ecolingo/Model/ChallengeModel.swift
+++ Ecolingo/Ecolingo/Model/ChallengeModel.swift
@@ -14,14 +14,21 @@ struct Challenge {
     let topic: ChallengeTopic
 }
 
-enum ChallengeType: String {
-    case watch, experience, play, challenge, unknown
+enum ChallengeType: String, CaseIterable, Identifiable {
+    case watch = "Watch"
+    case experience = "Experience"
+    case play = "Play"
+    case challenge = "Challenge"
+    
+    var id: String { self.rawValue }
 }
 
-enum ChallengeTopic: String {
+enum ChallengeTopic: String, CaseIterable, Identifiable {
     case plasticWaste = "Plastic Waste"
     case urbanFarming = "Urban Farming"
     case sustainableFashion = "Sustainable Fashion"
     case renewableEnergy = "Renewable Energy"
     case wildlifeConservation = "Wildlife Conservation"
+    
+    var id: String { self.rawValue }
 }
--- Ecolingo/Ecolingo/Views/HomeView.swift
+++ Ecolingo/Ecolingo/Views/HomeView.swift
@@ -8,15 +8,63 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationStack {
-            List {
-                Text("LIST OF TOPICS")
-                Text("TO DECIDE IN")
-                Text("NEW ISSUE")
-                
+        VStack {
+            HStack {
+                Text("Challenges")
+                    .font(.largeTitle)
+                    .underline()
+                Spacer()
+            }.padding(.horizontal, 30)
+            HStack {
+                ForEach(ChallengeType.allCases) { type in
+                    Button {
+                        
+                    } label: {
+                        Text(type.rawValue)
+                    }
+                    .buttonStyle(.bordered)
+                    .padding(.horizontal)
+                }
             }
-            .navigationTitle("Topics")
+            ScrollView {
+                ForEach(ChallengeTopic.allCases) { topic in
+                    VStack {
+                        Text(topic.rawValue)
+                            .font(.title)
+                        VStack (alignment: .leading) {
+                            HStack {
+                                Text("Challenge title, long text will be shown as this")
+                                    .multilineTextAlignment(.leading)
+                                    .lineLimit(2)
+                                    .truncationMode(.tail)
+                                    .font(.caption)
+                                    .padding(.trailing)
+                                Text("+5 XP")
+                                    .font(.footnote)
+                                    .bold()
+                                    .foregroundStyle(.green)
+                                    .padding(5)
+                                    .background(.ultraThinMaterial)
+                            }
+                            .frame(width: 200, height: 40)
+                            
+                            Image(systemName: "carrot.fill")
+                                .resizable()
+                                .foregroundStyle(.orange)
+                                .scaledToFit()
+                                .padding()
+                                .frame(height: 85)
+                                .background()
+                        }
+                        .frame(width: 220, height: 200)
+                        .clipShape(.rect(cornerRadius: 5))
+                        .background(.green.opacity(0.1))
+                        .shadow(radius: 5, x: 2, y: 2)
+                    }
+                }
+            }
         }
+        Spacer()
     }
 }
 
